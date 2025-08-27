335ca97d452a97ceca703ce4f716f943e70f2636
thisisjustausername <lpwgfs@gmail.com>
2025-08-27 22:15:44 +0000

    change and add tables

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,5 +1,7 @@
 -- Description: create tables, types, check functions for database
 
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 -- enum for user_role in table users
 CREATE TYPE USER_ROLE AS ENUM ('admin', 'host', 'guest', 'extern');
 
@@ -13,7 +15,7 @@ CREATE FUNCTION is_valid_invited_by_id(INTEGER) RETURNS boolean AS $$
 $$ LANGUAGE SQL;
 
 -- table to save users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     user_role USER_ROLE NOT NULL,
     room INTEGER CHECK ((user_role = 'extern' AND room IS NULL) OR (user_role != 'guest' AND room > 0)), 
@@ -23,21 +25,38 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'),
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, 
-    invited_by INTEGER REFERENCES users(id) CHECK (invited_by IS NULL OR (id != invited_by AND is_valid_invited_by_id(invited_by)))
+    invited_by INTEGER REFERENCES users(id) CHECK (invited_by IS NULL OR (id != invited_by AND is_valid_invited_by_id(invited_by))), 
+    personal_hash TEXT DEFAULT encode(gen_random_bytes(16), 'hex') UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stueble_motto (
+    id SERIAL PRIMARY KEY, 
+    motto TEXT NOT NULL,
+    date_of_time TIMESTAMPTZ NOT NULL, 
+    shared_apartment TEXT
+);
+
+CREATE TABLE IF NOT EXISTS stueble_codes (
+    id SERIAL PRIMARY KEY, 
+    userID INTEGER REFERENCES users(id) NOT NULL, 
+    code TEXT DEFAULT encode(gen_random_bytes(16), 'hex') UNIQUE NOT NULL, 
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP, 
+    expiration_date TIMESTAMPTZ NOT NULL, 
+    stueble_id INTEGER REFERENCES stueble_motto(id) NOT NULL
 );
 
 -- table to save login sessions
-CREATE TABLE sessions (
+CREATE TABLE IF NOT EXISTS sessions (
     id SERIAL PRIMARY KEY,
     expiration_date TIMESTAMPTZ NOT NULL, 
     userID INTEGER REFERENCES users(id) NOT NULL
 );
 
 -- table to save user and host events
-CREATE TABLE events (
+CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY, 
     userID INTEGER REFERENCES users(id) NOT NULL, 
     event_type EVENT_TYPE NOT NULL, 
     affected INTEGER REFERENCES users(id) NOT NULL,
     submitted TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
