4b8a8ec7d6fc90746f96a75c3dcef15b7c17bf44
thisisjustausername <lpwgfs@gmail.com>
2025-08-27 18:17:07 +0000

    work on db interaction

--- backend/sql_connection/database.py
+++ backend/sql_connection/database.py
@@ -15,10 +15,11 @@ def full_pack(func):
     """
     full_pack \n
     wrapper function to make just one function call for initializing, closing and the actual function
-    :param func:
-    :type func: function
-    :return: wrapped function
-    :rtype: function
+
+    Parameters:
+        func (func): function
+    Returns:
+        function: wrapped function
     """
     def wrapped(*args, **kwargs):
         conn, cursor = connect()
@@ -30,11 +31,11 @@ def full_pack(func):
 # TODO right now for development and testing enabled, for website rather use failesaves (SAME AS IN often_used_db_calls.py)
 def catch_exception(func):
     """
-    catch_exception \n
-    catches errors \n
+    catches errors
     connect, pool, update, remove functions DON'T use this wrapper
-    :param func:
-    :return:
+
+    Parameters:
+        func (func): function
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -49,9 +50,11 @@ def connect(**kwargs):
     connect \n
     kwargs format: user, password, host, port, database \n
     kwargs format if lazy: database (takes longer, so if called often use first option)
-    :param kwargs:
-    :type kwargs: dict
-    :return: connection, cursor
+
+    Parameters:
+        kwargs (dict):
+    Returns:
+        connection, cursor
     """
     if len(kwargs) > 0:
         conn = pg.connect(**kwargs)
@@ -59,17 +62,19 @@ def connect(**kwargs):
     conn = pg.connect(user=USER, password=PASSWORD, host=HOST,
                                  port=PORT, database=DBNAME)
     return conn, conn.cursor()
+    
 
 def create_pool(max_connections : int = 20):
     """
     create_pool \n
     creates a thread pool safely
-    :param max_connections:
-    :type max_connections: int
-    :return: connection_pool
-    """
 
-    connection_pool = pool.ThreadedConnectionPool(
+    Parameters:
+    max_connections (int):
+    Returns:
+        connection_pool: connection_pool
+    """
+    connection_pool = pool.ThreadedConctionPool(
         minconn=1,
         maxconn=max_connections,
         user=USER,
@@ -92,26 +97,20 @@ def read_table(cursor, table_name: str, keywords: list=["*"], conditions: dict={
     """
     read_table \n
     read data from a table
-    :param cursor: conn to db
-    :param table_name: table to query from
-    :type table_name: str
-    :param keywords: columns, that should be selected, if empty, get all
-    :type keywords: list
-    :param conditions: under which conditions (key: column, value: value) values should be selected, if empty, no conditions
-    :type conditions: dict
-    :param expect_single_answer: specify whether one or more answers are to be received, therefore it changes, whether list or single object will be returned
-    :type expect_single_answer: bool
-    :param select_max_of_key: conditions must be empty, otherwise it won't be used
-    :type select_max_of_key: bool
-    :param specific_where: select_max_key must be empty as well as conditions must be empty, else specific_where is ignored, allows to pass in a unique where statement (WHERE is already in the string)
-    :type specific_where: str
-    :param order_by: (key to order by, 0: descending / 1: ascending) by default no ordering, if specified and second value is invalid, then set to DESC
-    :type order_by: tuple
-    :param connection: is added to make using the wrapper full_pack easier
-    :type connection: connection
-    :return: {"success": bool, data: value}
-    :rtype: dict
+    
+    Parameters:
+        cursor (from): conn to db
+        keywords (list): columns, that should be selected, if empty, get all
+        conditions (dict): under which conditions (key: column, value: value) values should be selected, if empty, no conditions
+        expect_single_answer (bool): specify whether one or more answers are to be received, therefore it changes, whether list or single object will be returned
+        select_max_of_key (bool): conditions must be empty, otherwise it won't be used
+        specific_where (str): select_max_key must be empty as well as conditions must be empty, else specific_where is ignored, allows to pass in a unique where statement (WHERE is already in the string)
+        order_by (tuple): (key to order by, 0: descending / 1: ascending) by default no ordering, if specified and second value is invalid, then set to DESC
+        connection (connection): is added to make using the wrapper full_pack easier
+    Returns:
+        dict: {"success": bool, data: value}
     """
+
     query = f"""SELECT {', '.join(keywords)} FROM {table_name}"""
     if len(conditions) > 0:
         query += f" WHERE {' AND '.join([f'{key} = %s' for index, key in enumerate(conditions.keys())])}"
@@ -136,22 +135,18 @@ def read_table(cursor, table_name: str, keywords: list=["*"], conditions: dict={
 
 # NOTE arguments is either of type dict or of type list
 @catch_exception
-def insert_table(connection, cursor, table_name: str, arguments: dict = {}, returning: bool =True):
+def insert_table(connection, cursor, table_name: str, arguments: dict = {}, returning: bool = True):
     """
-    insert_table \n
     insert data into table
-    :param connection:  conn to db
-    :type connection: connection
-    :param cursor: cursor for interaction with db
-    :type cursor: cursor
-    :param table_name: table to insert into, if empty set all
-    :type table_name: str
-    :param arguments: values that should be entered (key: column, value: value), if empty, no conditions, if arguments is of type list, then list has to contain all values that have to be entered
-    :type arguments: dict, list
-    :param returning: returns the id of the added row
-    :type returning: bool
-    :return: {"success": bool, "data": id} by default, {"success": bool} if returning is False, {"success": False, "error": e} if error occured
-    :rtype: dict
+
+    Parameters:
+        connection (connection):  conn to db
+        cursor (cursor): cursor for interaction with db
+        table_name (str): table to insert into, if empty set all
+        arguments (list): values that should be entered (key: column, value: value), if empty, no conditions, if arguments is of type list, then list has to contain all values that have to be entered
+        returning (bool): returns the id of the added row
+    Returns:
+        dict: {"success": bool, "data": id} by default, {"success": bool} if returning is False, {"success": False, "error": e} if error occured
     """
     try:
         vals = []
@@ -179,28 +174,22 @@ def insert_table(connection, cursor, table_name: str, arguments: dict = {}, retu
 def update_table(connection, cursor, table_name: str, arguments: dict={}, conditions: dict={},
                  specific_where: str = "", specific_set: str = "", returning_column: str = ""):
     """
-    update_table \n
     updates values in a table \n
     already has try catch
-    :param connection:  conn to db
-    :type connection: connection
-    :param cursor: cursor to interact with db
-    :type cursor: cursor
-    :param table_name: table to insert into, if empty set all
-    :type table_name: str
-    :param arguments: values that should be entered (key: column, value: value)
-    :type arguments: dict
-    :param conditions: specify to insert into the correct row
-    :type conditions: dict
-    :param specific_where: conditions must be empty, otherwise conditions will be ignored, specifies where should be set, IMPORTANT what is being ignored differs from the other functions
-    :type specific_where: str
-    :param specific_set: arguments must be empty, otherwise arguments will be ignored, specifies what should be set
-    :type specific_set: str
-    :param returning_column: returns the specified column, returns just a single column
-    :type returning_column: str
-    :return: {"success": bool} by default, {"success": bool, data: value} if returning_column is filled, {"success": False, "error": e} if error occured
-    :rtype: dict
+
+    Parameters:
+        connection (connection):  conn to db
+        cursor (cursor): cursor to interact with db
+        table_name (str): table to insert into, if empty set all
+        arguments (dict): values that should be entered (key: column, value: value)
+        conditions (dict): specify to insert into the correct row
+        specific_where (str): conditions must be empty, otherwise conditions will be ignored, specifies where should be set, IMPORTANT what is being ignored differs from the other functions
+        specific_set (str): arguments must be empty, otherwise arguments will be ignored, specifies what should be set
+        returning_column (str): returns the specified column, returns just a single column
+    Returns:
+        dict: {"success": bool} by default, {"success": bool, data: value} if returning_column is filled, {"success": False, "error": e} if error occured
     """
+
     if arguments is None:
         arguments = {}
     try:
@@ -227,22 +216,19 @@ def update_table(connection, cursor, table_name: str, arguments: dict={}, condit
 
 def remove_table(connection, cursor, table_name: str, conditions: dict, returning_column: str = ""):
     """
-    remove_table \n
     removes data from table \n
     already has try catch
-    :param connection: conn to db
-    :type connection: connection
-    :param cursor: cursor to interact with db
-    :type cursor: cursor
-    :param table_name: table to insert into, if empty set all
-    :type table_name: str
-    :param conditions: specify from which row to remove the data
-    :type conditions: dict
-    :param returning_column: returns the specified column, returns just a single value
-    :type returning_column: str
-    :return: {"success": True} if successful, {"success": False, "error": e} else
-    :rtype dict
+
+    Parameters:
+        connection (connection): conn to db
+        cursor (cursor): cursor to interact with db
+        table_name (str): table to insert into, if empty set all
+        conditions (dict): specify from which row to remove the data
+        returning_column (str): returns the specified column, returns just a single value
+    Returns:
+        dict: {"success": True} if successful, {"success": False, "error": e} else
     """
+
     try:
         query = f"""DELETE FROM {table_name}
                     WHERE {' AND '.join(key + " = %s" for index, key in enumerate(conditions))}"""
@@ -260,19 +246,16 @@ def remove_table(connection, cursor, table_name: str, conditions: dict, returnin
 
 def custom_call(connection, cursor, query: str, type_of_answer: int, variables: list=None):
     """
-    custom_call \n
     send a custom query to the database
-    :param connection: can be None if it isn't needed (e.g. for SELECT statements)
-    :type connection: connection
-    :param cursor:
-    :type cursor: cursor
-    :param query:
-    :type query: str
-    :param type_of_answer: -1 -> no answer is being expected, 0 -> single answer is being expected, 1 -> list of answers is being expected
-    :type type_of_answer: bool
-    :param variables: list of variables that should be passed into the query
-    :type variables: list
-    :return: None, single variable, list of variables: depending on type_of_answer
+
+    Parameters:
+    connection (connection): can be None if it isn't needed (e.g. for SELECT statements)
+    cursor (cursor):
+    query (str):
+    type_of_answer (bool): -1 -> no answer is being expected, 0 -> single answer is being expected, 1 -> list of answers is being expected
+    variables (list): list of variables that should be passed into the query
+    Returns:
+        :: None, single variable, list of variables: depending on type_of_answer
     """
     try:
         if variables is None:
@@ -301,14 +284,13 @@ def custom_call(connection, cursor, query: str, type_of_answer: int, variables: 
 @catch_exception
 def get_time(cursor, connection=None):
     """
-    get_time \n
     returns the current berlin time
-    :param cursor: cursor to interact with db
-    :type cursor: cursor
-    :param connection: is added to make using the wrapper full_pack easier
-    :type connection: connection
-    :return: {"success": True, "data": data}
-    :rtype: dict
+
+    Parameters:
+        cursor (cursor): cursor to interact with db
+        connection (connection): is added to make using the wrapper full_pack easier
+    Returns:
+        dict: {"success": True, "data": data}
     """
     query = """SELECT NOW() AT TIME ZONE 'Europe/Berlin' AS current_time"""
     cursor.execute(query)
@@ -317,11 +299,10 @@ def get_time(cursor, connection=None):
 
 def close(connection, cursor=None):
     """
-    close \n
     closes the current cursor and connection
-    :param connection: connection that should be closed
-    :param cursor: cursor that should be closed
-    :return: void
+    
+    Parameters:
+        connection (closed): connection that should be closed
     """
     connection.close()
     if cursor is not None:
