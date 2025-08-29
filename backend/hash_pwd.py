54988db02727eba1aa639931c76438c47cd8bb29
thisisjustausername <lpwgfs@gmail.com>
2025-08-29 15:00:43 +0200

    fix bugs

--- backend/hash_pwd.py
+++ backend/hash_pwd.py
@@ -1,7 +1,9 @@
 import bcrypt
 
 def hash_pwd(password: str) -> str:
+    password = password.encode()
     return bcrypt.hashpw(password, bcrypt.gensalt()).decode()
 
 def match_pwd(password: str, hashed: str) -> bool:
+    password = password.encode()
     return bcrypt.checkpw(password, hashed.encode())
