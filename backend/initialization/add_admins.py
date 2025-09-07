f8baad08060c926a88afbf4c95d98fb60746bc87
thisisjustausername <lpwgfs@gmail.com>
2025-09-07 20:57:33 +0200

    work on comments, create initialization folder

---
+++ backend/initialization/add_admins.py
@@ -0,0 +1,24 @@
+from backend.sql_connection import database as db
+
+conn, cursor = db.connect()
+
+result = db.insert_table(
+    connection=conn,
+    cursor=cursor,
+    table_name="users",
+    arguments=["user_role", "room", "residence", "first_name", "last_name", "email", "user_name", "password_hash"],
+    values=["admin", 0, "altbau", "Altbau", "Admin", "tutorenhes@gmail.com", "admin altbau", "tutorenhes2025"],
+    returning_column="id")
+if result["success"] is False:
+    raise result["error"]
+
+result = db.insert_table(
+    connection=conn,
+    cursor=cursor,
+    table_name="users",
+    arguments=["user_role", "room", "residence", "first_name", "last_name", "email", "user_name", "password_hash"],
+    values=["admin", 0, "hirte", "Hirte", "Admin", "tutorenhirte@gmail.com", "admin hirte", "tutorenhirte2025"],
+    returning_column="id")
+
+if result["success"] is False:
+    raise result["error"]
