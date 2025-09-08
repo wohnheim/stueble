157cefa18dc66658ddac24431b643747da203de8
thisisjustausername <lpwgfs@gmail.com>
2025-09-08 22:44:55 +0200

    fix bug

--- backend/sql_connection/common_functions.py
+++ backend/sql_connection/common_functions.py
@@ -1,2 +1,4 @@
 def clean_single_data(data: dict):
-    return {k: v if k != "data" else v[0] for k, v in data.items()}
+    if isinstance(data, dict):
+        return {k: v if k != "data" else v[0] for k, v in data.items()}
+    return data[0]
