be2a94fd63d5522eb5a588c0d32d8c3bac6ae1c4
thisisjustausername <lpwgfs@gmail.com>
2025-09-08 22:21:54 +0200

    fix bug

--- data/triggers.sql
+++ data/triggers.sql
@@ -71,7 +71,9 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER event_change_user_trigger
 AFTER UPDATE OR DELETE ON users
 FOR EACH ROW
-WHEN (OLD.* IS DISTINCT FROM NEW.* AND ((OLD.last_updated IS DISTINCT FROM NEW.last_updated) IS FALSE))
+WHEN (
+    TG_OP = 'DELETE'
+    OR (TG_OP = 'UPDATE' AND (OLD.* IS DISTINCT FROM NEW.* AND ((OLD.last_updated IS DISTINCT FROM NEW.last_updated) IS FALSE)))
 EXECUTE FUNCTION event_change_user();
 
 CREATE OR REPLACE TRIGGER event_add_user_trigger
