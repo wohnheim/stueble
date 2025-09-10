e9c4b209e83929a0d1464ae22d86accef9f9233b
thisisjustausername <lpwgfs@gmail.com>
2025-09-10 13:16:38 +0200

    add files

---
+++ backend/sql_connection/events.py
@@ -0,0 +1,35 @@
+from click import argument
+
+import backend.sql_connection.database as db
+from backend.sql_connection.common_functions import clean_single_data
+
+
+def add_guest(connection, cursor, user_id: int, stueble_id: int, invited_by: int | None=None) -> dict:
+    """
+    adds a guest to the table events with event_type "add"
+
+    Parameters:
+        connection: connection to the db
+        cursor: cursor for the connection
+        user_id (int): id of the user
+        stueble_id (int): id of the stueble party
+        invited_by (int | None): id of the invited user
+    Returns:
+        dict: {"success": bool} by default, {"success": bool, "data": id} if returning is True, {"success": False, "error": e} if error occurred
+    """
+
+    arguments = {"user_id": user_id, "stueble_id": stueble_id, "event_type": "add"}
+    if invited_by is not None:
+        arguments["invited_by"] = invited_by
+
+    result = db.insert_table(
+        connection=connection,
+        cursor=cursor,
+        table_name="events",
+        arguments=arguments,
+        returning_column="NOW()")
+
+    # maybe shouldn't be possible, but still left in
+    if result["success"] and result["data"] is None:
+        return {"success": False, "error": "error occurred"}
+    return clean_single_data(result)
