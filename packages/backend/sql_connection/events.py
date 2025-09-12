0e224beb0c57f17f8ac3fe85cfa531e059b8f723
thisisjustausername <lpwgfs@gmail.com>
2025-09-12 19:04:32 +0200

    fix bugs, work on websocket

--- packages/backend/sql_connection/events.py
+++ packages/backend/sql_connection/events.py
@@ -1,4 +1,4 @@
-from packages import backend as db
+from packages.backend.sql_connection import database as db
 from packages.backend.sql_connection.common_functions import clean_single_data
 
 
@@ -25,6 +25,31 @@ def add_guest(connection, cursor, user_id: int, stueble_id: int, invited_by: int
         cursor=cursor,
         table_name="events",
         arguments=arguments,
+        returning_column="NOW()")
+
+    # maybe shouldn't be possible, but still left in
+    if result["success"] and result["data"] is None:
+        return {"success": False, "error": "error occurred"}
+    return clean_single_data(result)
+
+def remove_guest(connection, cursor, user_id: int, stueble_id: int) -> dict:
+    """
+    adds a guest to the table events with event_type "remove" effectively removing them from the guest list
+
+    Parameters:
+        connection: connection to the db
+        cursor: cursor for the connection
+        user_id (int): id of the user
+        stueble_id (int): id of the stueble party
+    Returns:
+        dict: {"success": bool} by default, {"success": bool, "data": id} if returning is True, {"success": False, "error": e} if error occurred
+    """
+
+    result = db.insert_table(
+        connection=connection,
+        cursor=cursor,
+        table_name="events",
+        arguments={"user_id": user_id, "stueble_id": stueble_id, "event_type": "remove"},
         returning_column="NOW()")
 
     # maybe shouldn't be possible, but still left in
