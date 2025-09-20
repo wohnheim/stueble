510a117ec93c3249b02f595b655196f3a3bba523
thisisjustausername <lpwgfs@gmail.com>
2025-09-21 01:43:11 +0200

    fix bug

--- packages/backend/initialization/configurations.py
+++ packages/backend/initialization/configurations.py
@@ -39,7 +39,7 @@ set_key(env_file_path, "PUBLIC_KEY", public_key_string)
 
 set_key(env_file_path, "USERDB", "stueble")
 set_key(env_file_path, "HOST", "localhost")
-set_key(env_file_path, "PORT", 5432)
+set_key(env_file_path, "PORT", "5432")
 set_key(env_file_path, "DBNAME", "stueble_data")
 
 inputted_data = input("Enter the password for the postgres user 'stueble': ")
