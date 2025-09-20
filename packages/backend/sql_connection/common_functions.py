d6989096491a1f86cbf7673c46381118c570b73f
thisisjustausername <lpwgfs@gmail.com>
2025-09-20 14:56:17 +0200

    change file structure, avoid circular imports

--- packages/backend/sql_connection/common_functions.py
+++ packages/backend/sql_connection/common_functions.py
@@ -1,2 +1,44 @@
-def clean_single_data(data: dict):
-    return {k: v if k != "data" else v[0] for k, v in data.items()}
+from packages.backend.sql_connection import sessions, database as db
+from packages.backend.data_types import *
+
+pool = db.create_pool()
+
+def get_conn_cursor():
+    """
+    gets a connection and a cursor from the connection pool
+    """
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    return conn, cursor
+
+def close_conn_cursor(connection, cursor):
+    """
+    closes the cursor and returns the connection to the pool
+    """
+    cursor.close()
+    pool.putconn(connection)
+
+def check_permissions(cursor, session_id: str, required_role: UserRole) -> dict:
+    """
+    checks whether the user with the given session_id has the required role
+    Parameters:
+        cursor: cursor for the connection
+        session_id (str): session id of the user
+        required_role (UserRole): required role of the user
+    Returns:
+        dict: {"success": bool, "data": {"allowed": bool, "user_id": int, "user_role": UserRole}, {"success": False, "error": e} if error occurred
+    """
+
+    # get the user_id, user_role by session_id
+    result = sessions.get_user(cursor=cursor, session_id=session_id)
+
+    # if error occurred, return error
+    if result["success"] is False:
+        return result
+    user_id = result["data"][0]
+    user_role = result["data"][1]
+    user_role = UserRole(user_role)
+    user_uuid = result["data"][2]
+    if user_role >= required_role:
+        return {"success": True, "data": {"allowed": True, "user_id": user_id, "user_role": user_role, "user_uuid": user_uuid}}
+    return {"success": True, "data": {"allowed": False, "user_id": user_id, "user_role": user_role, "user_uuid": user_uuid}}
