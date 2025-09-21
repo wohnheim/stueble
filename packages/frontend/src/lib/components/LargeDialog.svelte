353c60816c0accaddc6326bf47759a7a5fee610a
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2025-09-21 23:50:45 +0200

    fix: remove infinite loop

--- packages/frontend/src/lib/components/LargeDialog.svelte
+++ packages/frontend/src/lib/components/LargeDialog.svelte
@@ -2,12 +2,18 @@
   import { onMount } from "svelte";
 
   import type { Host } from "$lib/api/types";
+  import { database } from "$lib/lib/database.svelte";
   import { ui_object } from "$lib/lib/UI.svelte";
 
   import Hosts from "$lib/pages/Hosts.svelte";
 
   let hostsPage = $state<"list" | "add">("list");
-  let hostsSelected = $state<Host[]>([]);
+  let hostsSelectedUnfiltered = $state<Host[]>([]);
+  let hostsSelected = $derived(
+    hostsSelectedUnfiltered.filter(
+      (u) => !database.hosts.some((h) => u.id == h.id),
+    ),
+  );
 
   onMount(() => {
     ui_object.largeDialog?.addEventListener("close", () => {
@@ -25,7 +31,11 @@
 
 <dialog id="dialog-large" bind:this={ui_object.largeDialog} class="right large">
   {#if ui_object.path.main == "settings"}
-    <Hosts bind:page={hostsPage} bind:selected={hostsSelected} />
+    <Hosts
+      bind:page={hostsPage}
+      bind:selectedUnfiltered={hostsSelectedUnfiltered}
+      bind:selected={hostsSelected}
+    />
   {/if}
 </dialog>
 
