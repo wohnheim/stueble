eef3a49ac338345f5ffdb360909d2665aeca5112
thisisjustausername <lpwgfs@gmail.com>
2025-10-01 01:18:26 +0200

    add html to email

--- packages/backend/google_functions/email.py
+++ packages/backend/google_functions/email.py
@@ -1,5 +1,7 @@
 import smtplib
 from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from packages.backend.data_types import Email
 from dotenv import load_dotenv
 import os
@@ -10,21 +12,26 @@ load_dotenv(env_file_path)
 EMAIL_ADDRESS = "stuebleheshirte@gmail.com"
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-def send_mail(recipient: Email, subject: str, body: str):
+def send_mail(recipient: Email, subject: str, body: str, html: bool=False):
     """
     Sends an email message.
     Parameters:
         recipient (Email): The recipient's email address.
         subject (str): The subject of the email.
         body (str): The body content of the email.
+        html (bool): Whether the email is html or not.
     """
 
-    msg = EmailMessage()
-    msg.set_content(body)
+    msg = EmailMessage() if not html else MIMEMultipart("alternative")
     msg["Subject"] = subject
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = recipient.email
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    if not html:
+        msg.set_content(body)
+    else:
+        msg.attach(MIMEText(body, "html"))
+    with smtplib.SMTP_SSL("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
     return {"success": True}
