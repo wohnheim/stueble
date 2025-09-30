b37aa7cb0a034e63ee5d585203bcdbb3c663062d
Quentin Frey <51170829+Limosine@users.noreply.github.com>
2025-09-30 21:21:42 +0200

    fix: button visibility

--- packages/frontend/src/lib/dialogs/Confirm.svelte
+++ packages/frontend/src/lib/dialogs/Confirm.svelte
@@ -10,6 +10,10 @@
 
 <p style="font-size: large;">{properties.title}</p>
 
+{#if properties.description !== undefined}
+  <p>{properties.description}</p>
+{/if}
+
 <nav class="right-align" style="padding: 10px 0 0 0;">
   <button class="transparent link" onclick={() => ui_object.closeDialog()}>
     {properties.cancel === undefined ? "Cancel" : properties.cancel}
