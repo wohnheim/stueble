453b9e89434eb0592e729f31b8265a7cab1531c8
thisisjustausername <lpwgfs@gmail.com>
2025-09-30 18:20:41 +0200

    change google login to service account

--- packages/backend/google_functions/authentification.py
+++ packages/backend/google_functions/authentification.py
@@ -1,7 +1,6 @@
 import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 def authenticate():
     """
@@ -12,21 +11,10 @@ def authenticate():
     # created automatically when the authorization flow completes for the first
     # time.
     SCOPES = ["https://www.googleapis.com/auth/gmail.send", "https://www.googleapis.com/auth/drive"]
+    SERVICE_ACCOUNT_FILE = "do_not_track/credentials/credentials.json"
 
-    if os.path.exists("do_not_track/credentials/token.json"):
-        creds = Credentials.from_authorized_user_file("do_not_track/credentials/token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "do_not_track/credentials/credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("do_not_track/credentials/token.json", "w") as token:
-            token.write(creds.to_json())
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     return creds
 
 if __name__ == "__main__":
