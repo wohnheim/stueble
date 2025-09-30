28381f40162430ea4748ddd5fc3b018ecb43c648
thisisjustausername <lpwgfs@gmail.com>
2025-10-01 01:21:15 +0200

    fix email

--- packages/backend/initialization/add_admins.py
+++ packages/backend/initialization/add_admins.py
@@ -15,7 +15,7 @@ password_hes = hash_pwd.hash_pwd(pwd_hes)
 result = db.insert_table(
     cursor=cursor,
     table_name="users",
-    arguments={"user_role":"admin",  "room": 0, "residence": "altbau", "first_name": "Super", "last_name": "Admin", "email": "tutorenhes@gmail.com", "user_name": "admin", "password_hash": password_hes},
+    arguments={"user_role":"admin",  "room": 0, "residence": "altbau", "first_name": "Super", "last_name": "Admin", "email": "tutorenheshirte@gmail.com", "user_name": "admin", "password_hash": password_hes},
     returning_column="id")
 if result["success"] is False:
     raise result["error"]
