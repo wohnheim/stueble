c2d35a33fbeb9f5e1e3d5f977f38428c072e6050
thisisjustausername <lpwgfs@gmail.com>
2025-10-15 04:33:25 +0200

    fix bugs

--- packages/data/cron_job_remove_hosts.sql
+++ packages/data/cron_job_remove_hosts.sql
@@ -1,5 +1,7 @@
 SELECT cron.schedule(
-  '0 7 * * 4', -- every Thursday at 7am
+  '51 5 * * 4', -- every Thursday at 5:51am
   $$UPDATE users SET user_role = 'user'
-    WHERE user_role = 'host';$$);
- 
+    WHERE user_role = 'host';
+    $$);
+
+  -- TODO: UPDATE users SET user_role = 'host' WHERE user_role = 'user' AND 
