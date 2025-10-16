c6552e4a276743c71fd8c77a33601508aac00aa3
thisisjustausername <lpwgfs@gmail.com>
2025-10-16 09:32:22 +0200

    fix cronjob

--- packages/data/cron_job_create_stueble.sql
+++ packages/data/cron_job_create_stueble.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION create_stueble_motto_if_not_exists()
 RETURNS void AS $$
 DECLARE next_wednesday DATE;
 BEGIN
-next_wednesday := CURRENT_DATE + ((3 - EXTRACT(DOW FROM CURRENT_DATE) + 7) % 7) AS next_wednesday;
+next_wednesday := CURRENT_DATE + ((2 + EXTRACT(DOW FROM CURRENT_DATE)) % 7) * INTERVAL '1 day' AS next_wednesday;
 IF NOT EXISTS (SELECT id FROM stueble_motto WHERE date_of_time = next_wednesday) THEN
     INSERT INTO stueble_motto (motto, date_of_time)
     VALUES ('', next_wednesday);
